ad74c2431088ea7fcbbab3a411a1ce9d2e8399ae
Guest 01 <guest01@alunos.sc.senac.br>
2022-06-21 19:04:10 -0300

    typescript

---
+++ typescript/2106/caculos.ts
@@ -0,0 +1,7 @@
+function somar(n1: number, n2: number):number{
+    return n1 + n2
+}
+
+function subtrair(n1: number, n2: number):number{
+    return n1 - n2
+}
---
+++ typescript/2106/strings/caculos.js
@@ -0,0 +1,7 @@
+"use strict";
+function somar(n1, n2) {
+    return n1 + n2;
+}
+function subtrair(n1, n2) {
+    return n1 - n2;
+}
