22bfcea38c83e516bbc341589d421e24dbe68dcb
Guest 01 <guest01@alunos.sc.senac.br>
2022-06-21 19:18:45 -0300

    typescript

--- typescript/2106/caculos.ts
+++ typescript/2106/caculos.ts
@@ -5,3 +5,9 @@ function somar(n1: number, n2: number):number{
 function subtrair(n1: number, n2: number):number{
     return n1 - n2
 }
+function dividir(n1: number, n2: number):number{
+    return n1 / n2
+}
+function multiplicar(n1: number, n2: number):number{
+    return n1 * n2
+}
