d23bffb041f0661f2b92f1810bffdd55af357c5f
Guest 01 <guest01@alunos.sc.senac.br>
2022-06-21 18:29:19 -0300

    typescript

--- typescript/carrinho.ts
+++ typescript/carrinho.ts
@@ -12,9 +12,9 @@ interface Icarrinho {
     qtd: number
     valor: number
     descricao: string
-}*//
+}*/
 
-var carrinho: Icarrinho = {id:1,produto[qtd:2,valor:29,descricao:"arroz branco"]}
+var carrinho: Icarrinho = {id:1,produto:[{qtd:2,valor:29,descricao:arrozbranco;}]}
 
 
 
