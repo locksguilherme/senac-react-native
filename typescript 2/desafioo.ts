8a68c23959d341ee028d8ab35b05b47c468cccf2
Guest 01 <guest01@alunos.sc.senac.br>
2022-06-22 20:57:14 -0300

    typescript

--- typescript 2/desafioo.ts
+++ typescript 2/desafioo.ts
@@ -5,3 +5,7 @@ let p1 = new Pessoa (1,'Jonas','57774', 'jonas@gamil.com', 50, 1.55)
 p1.imc()
 p1.imprimir()
 
+let p2 = new Pessoa(2,'Giovana','45454564','jooj@gmail.com', 75, 1.88)
+
+p2.imc()
+p2.imprimir()
--- typescript 2/scripts/desafioo.js
+++ typescript 2/scripts/desafioo.js
@@ -7,3 +7,6 @@ const desfio_1 = __importDefault(require("./desfio"));
 let p1 = new desfio_1.default(1, 'Jonas', '57774', 'jonas@gamil.com', 50, 1.55);
 p1.imc();
 p1.imprimir();
+let p2 = new desfio_1.default(2, 'Giovana', '45454564', 'jooj@gmail.com', 75, 1.88);
+p2.imc();
+p2.imprimir();
